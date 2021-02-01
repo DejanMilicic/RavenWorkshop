9b3ca8e65986b60980e5e8899d943889f46ddd5c
Dejan Milicic <dejan.milicic@gmail.com>
2021-02-01 11:43:39 +0100

    Empty console project

---
+++ Northwind/Northwind/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Northwind
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
