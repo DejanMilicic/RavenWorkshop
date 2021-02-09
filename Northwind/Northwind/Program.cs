55e1b9a8cbde328df18339d704553a3bf62658dc
Dejan Milicic <dejan.milicic@gmail.com>
2021-02-09 20:41:38 +0100

    All Employees in London

--- Northwind/Northwind/Program.cs
+++ Northwind/Northwind/Program.cs
@@ -180,6 +180,24 @@ namespace Northwind
             //Console.WriteLine(sp.Elapsed);
 
             #endregion
+
+            #region All Employees in London
+
+            //from Employees
+            //where Address.City = "London"
+
+            using var session = store.OpenSession();
+
+            var employees = session.Query<Employee>()
+                .Where(x => x.Address.City == "London")
+                .ToList();
+
+            foreach (Employee employee in employees)
+            {
+                Console.WriteLine(employee.FirstName + " " + employee.LastName);
+            }
+
+            #endregion
         }
     }
 }
