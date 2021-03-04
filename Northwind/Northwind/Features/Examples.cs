11b1574b4bbc749476181720d9bc75157f95a57c
Dejan Milicic <dejan.milicic@gmail.com>
2021-03-04 20:45:25 +0100

    Include via path

---
+++ Northwind/Northwind/Features/Examples.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using Raven.Client.Documents;
+using Raven.Client.Documents.Indexes;
+
+namespace Northwind.Features
+{
+    public partial class Examples
+    {
+        public readonly IDocumentStore store;
+
+        public Examples()
+        {
+            store = new DocumentStore
+            {
+                Urls = new string[] { "https://a.free.dejanmilicic.ravendb.cloud/" },
+                Certificate = new X509Certificate2("free.dejanmilicic.client.certificate.pfx"),
+                Database = "demo"
+            }.Initialize();
+
+            IndexCreation.CreateIndexes(typeof(Program).Assembly, store);
+        }
+    }
+}
---
+++ Northwind/Northwind/Features/Includes/Examples.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+using Northwind.Models.Entity;
+using Raven.Client.Documents;
+
+namespace Northwind.Features
+{
+    public partial class Examples
+    {
+        public void IncludeViaPath()
+        {
+            using var session = store.OpenSession();
+
+            var orders = session.Query<Order>()
+                .Include("Employee")
+                .Include("Company")
+                .Where(order => order.Company == "Companies/2-A")
+                .OrderByDescending(order => order.OrderedAt)
+                .Take(10)
+                .ToList();
+
+            foreach (Order order in orders)
+            {
+                var employee = session.Load<Employee>(order.Employee);
+                var company = session.Load<Company>(order.Company);
+                Console.WriteLine($"Order: {order.Id} \t {order.OrderedAt} \t via {employee.FirstName} \t for {company.Name}");
+            }
+
+            Console.WriteLine($"Total number of requests: {session.Advanced.NumberOfRequests}");
+        }
+    }
+}
