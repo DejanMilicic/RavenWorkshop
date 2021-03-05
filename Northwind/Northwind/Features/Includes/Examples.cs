6612bd41edaa3d1960466cbc03846b74d73c4003
Dejan Milicic <dejan.milicic@gmail.com>
2021-03-05 11:17:21 +0100

    Passing includes as a list of fields

--- Northwind/Northwind/Features/Includes/Examples.cs
+++ Northwind/Northwind/Features/Includes/Examples.cs
@@ -28,5 +28,30 @@ namespace Northwind.Features
 
             Console.WriteLine($"Total number of requests: {session.Advanced.NumberOfRequests}");
         }
+
+        public void IncludeViaPathList(string[] includes)
+        {
+            using var session = store.OpenSession();
+
+            var query = session.Query<Order>();
+
+            foreach (string incl in includes)
+                query = query.Include(incl);
+
+            var orders = query
+                .Where(order => order.Company == "Companies/2-A")
+                .OrderByDescending(order => order.OrderedAt)
+                .Take(10)
+                .ToList();
+
+            foreach (Order order in orders)
+            {
+                var employee = session.Load<Employee>(order.Employee);
+                var company = session.Load<Company>(order.Company);
+                Console.WriteLine($"Order: {order.Id} \t {order.OrderedAt} \t via {employee.FirstName} \t for {company.Name}");
+            }
+
+            Console.WriteLine($"Total number of requests: {session.Advanced.NumberOfRequests}");
+        }
     }
 }
