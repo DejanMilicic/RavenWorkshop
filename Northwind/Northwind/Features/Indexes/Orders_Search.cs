4fd4ab485499853b7b2ab2a8e1f50735856714db
Dejan Milicic <dejan.milicic@gmail.com>
2021-03-06 16:01:31 +0100

    Searching orders over orderlines product names

---
+++ Northwind/Northwind/Features/Indexes/Orders_Search.cs
@@ -0,0 +1,29 @@
+﻿using Northwind.Models.Entity;
+using Raven.Client.Documents.Indexes;
+using System.Linq;
+
+namespace Northwind.Features.Indexes
+{
+    public class Orders_Search : AbstractIndexCreationTask<Order, Orders_Search.Entry>
+    {
+        public class Entry
+        {
+            public string[] Query { get; set; }
+        }
+
+        public Orders_Search()
+        {
+            Map = orders => from order in orders
+                            from orderLine in order.Lines
+                            select new Entry
+                            {
+                                Query = new []
+                                {
+                                    orderLine.ProductName
+                                }
+                            };
+
+            Index(x => x.Query, FieldIndexing.Search);
+        }
+    }
+}
---
+++ Northwind/Northwind/Features/Search.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Northwind.Features.Indexes;
+using Northwind.Models.Entity;
+using Raven.Client.Documents;
+
+namespace Northwind.Features
+{
+    public partial class Examples
+    {
+        public void SearchOrderLines(string term)
+        {
+            using var session = store.OpenSession();
+
+            var res = session.Query<Orders_Search.Entry, Orders_Search>()
+                .Search(x => x.Query, term)
+                .OfType<Order>()
+                .ToList();
+
+            foreach (Order order in res)
+            {
+                Console.WriteLine($"{order.Id}");
+            }
+        }
+    }
+}
