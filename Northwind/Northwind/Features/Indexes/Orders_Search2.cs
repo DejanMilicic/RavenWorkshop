2bf415099ff6a21e8312e6ba2b67d486b9d69eef
Dejan Milicic <dejan.milicic@gmail.com>
2021-03-13 23:18:07 +0100

    order search 2

---
+++ Northwind/Northwind/Features/Indexes/Orders_Search2.cs
@@ -0,0 +1,34 @@
+﻿using Northwind.Models.Entity;
+using Raven.Client.Documents.Indexes;
+using System.Linq;
+using Raven.Client.Documents.Queries;
+
+namespace Northwind.Features.Indexes
+{
+    public class Orders_Search2 : AbstractIndexCreationTask<Order, Orders_Search2.Entry>
+    {
+        public class Entry
+        {
+            public string[] Query { get; set; }
+        }
+
+        public Orders_Search2()
+        {
+            Map = orders => from order in orders
+                            from orderLine in order.Lines
+
+                            let product = LoadDocument<Product>(orderLine.Product)
+
+                            select new Entry
+                            {
+                                Query = new []
+                                {
+                                    order.ShipTo.City,
+                                    product.Name
+                                }
+                            };
+
+            Index(x => x.Query, FieldIndexing.Search);
+        }
+    }
+}
