bcc0c12dd6efacc44c26cf92f32821c2b3f1ee57
Dejan Milicic <dejan.milicic@gmail.com>
2021-03-16 00:59:28 +0100

    Counters index

---
+++ Northwind/Northwind/Features/Counters/Employees_ByAttachmentName.cs
@@ -0,0 +1,20 @@
+﻿using Northwind.Models.Entity;
+using System.Linq;
+using Raven.Client.Documents.Indexes.Counters;
+
+namespace Northwind.Features.Attachments
+{
+    public class Employees_ByLikes : AbstractCountersIndexCreationTask<Employee>
+    {
+        public Employees_ByLikes()
+        {
+            AddMap("Likes", counters => from counter in counters
+                select new
+                {
+                    Likes = counter.Value,
+                    Name = counter.Name,
+                    Employee = counter.DocumentId
+                });
+        }
+    }
+}
