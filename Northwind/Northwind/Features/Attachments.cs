0862f6d79c5fa1e6232c7368de3208ddc57b9db8
Dejan Milicic <dejan.milicic@gmail.com>
2021-03-15 23:48:03 +0100

    get attachment names

---
+++ Northwind/Northwind/Features/Attachments.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Northwind.Models.Entity;
+using Raven.Client.Documents.Operations.Attachments;
+
+namespace Northwind.Features
+{
+    public class Attachments
+    {
+        public void GetAttachmentNames()
+        {
+            using var session = DocumentStoreHolder.Store.OpenSession();
+
+            var employee = session.Load<Employee>("employees/8-A");
+            var attNames = session.Advanced.Attachments.GetNames(employee);
+
+            foreach (AttachmentName attName in attNames)
+            {
+                Console.WriteLine(attName.Name);
+            }
+        }
+    }
+}
