acaab2b61f70c24cff7373f8aedabeef258322de
Dejan Milicic <dejan.milicic@gmail.com>
2021-03-15 23:47:53 +0100

    document store holder

---
+++ Northwind/Northwind/DocumentStoreHolder.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Raven.Client.Documents;
+
+namespace Northwind
+{
+    public static class DocumentStoreHolder
+    {
+        private static readonly Lazy<IDocumentStore> LazyStore =
+            new Lazy<IDocumentStore>(() =>
+            {
+                var store = new DocumentStore
+                {
+                    Urls = new[] { "http://127.0.0.1:8080" },
+                    Database = "demo"
+                };
+
+                return store.Initialize();
+            });
+
+        public static IDocumentStore Store => LazyStore.Value;
+    }
+}
