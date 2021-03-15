deb3b21d482d4a02f584b1b1cfb34d4048441d68
Dejan Milicic <dejan.milicic@gmail.com>
2021-03-16 00:39:12 +0100

    Employees by attachment name

---
+++ Northwind/Northwind/Features/Attachments/Employees_ByAttachmentName.cs
@@ -0,0 +1,28 @@
+﻿using Northwind.Models.Entity;
+using Raven.Client.Documents.Indexes;
+using System.Linq;
+
+namespace Northwind.Features.Attachments
+{
+    public class Employees_ByAttachmentName : AbstractIndexCreationTask<Employee, Employees_ByAttachmentName.Entry>
+    {
+        public class Entry
+        {
+            public string[] Query { get; set; }
+        }
+
+        public Employees_ByAttachmentName()
+        {
+            Map = employees => from employee in employees
+
+                let attachmentNames = AttachmentsFor(employee)
+
+                select new Entry
+                {
+                    Query = attachmentNames.Select(x => x.Name).ToArray()
+                };
+
+            Index(x => x.Query, FieldIndexing.Search);
+        }
+    }
+}
