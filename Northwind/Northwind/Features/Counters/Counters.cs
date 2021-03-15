0baf039fbfbed2f558041d8bb9c45c6d6d3e8002
Dejan Milicic <dejan.milicic@gmail.com>
2021-03-16 00:47:47 +0100

    get counters

---
+++ Northwind/Northwind/Features/Counters/Counters.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Raven.Client.Documents.Session;
+
+namespace Northwind.Features.Counters
+{
+    public class Counters
+    {
+        public void GetCounters()
+        {
+            using var session = DocumentStoreHolder.Store.OpenSession();
+
+            ISessionDocumentCounters counters = session.CountersFor("products/74-A");
+
+            string twoStars = counters.Get("⭐⭐").ToString();
+            
+            Console.WriteLine(twoStars);
+        }
+
+
+
+
+        //counters.Increment("⭐⭐");
+        //counters.Increment("⭐⭐⭐", -1);
+        //session.SaveChanges();
+    }
+}
