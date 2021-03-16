fb88b111db8f869c8fcf1b79b0a5d87f44c4387c
Dejan Milicic <dejan.milicic@gmail.com>
2021-03-16 23:08:20 +0100

    recursive index

---
+++ Northwind/Northwind/Features/Recursive/Employees_Managers.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Northwind.Models.Entity;
+using Raven.Client.Documents.Indexes;
+
+namespace Northwind.Features.Recursive
+{
+    public class Employees_Managers : AbstractIndexCreationTask<Employee, Employees_Managers.Entry>
+    {
+        public class Entry
+        {
+            public IEnumerable<string> Managers { get; set; }
+        }
+
+        public Employees_Managers()
+        {
+            Map = employees => from e in employees
+                let managersOfe = Recurse(
+                    LoadDocument<Employee>(e.ReportsTo),
+                    empl => LoadDocument<Employee>(empl.ReportsTo)
+                    )
+                select new
+                {
+                    managers = managersOfe.Select(x => x.FirstName)
+                };
+        }
+    }
+}
---
+++ Northwind/Northwind/Features/Recursive/Recursive.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Northwind.Models.Entity;
+using Raven.Client.Documents;
+using Raven.Client.Documents.Linq;
+
+namespace Northwind.Features.Recursive
+{
+    public class Recursive
+    {
+        public void ManagerSearch()
+        {
+            using var session = DocumentStoreHolder.Store.OpenSession();
+
+            string managerName = "Andrew";
+
+            var res = session.Advanced.RawQuery<Employee>(@$"
+                    from index 'Employees/Managers' where managers = '{managerName}' 
+                ").ToList();
+
+            Console.WriteLine($"{managerName} manages \n");
+
+            foreach (Employee employee in res)
+            {
+                Console.WriteLine($"{employee.FirstName} {employee.LastName}");
+            }
+        }
+    }
+}
