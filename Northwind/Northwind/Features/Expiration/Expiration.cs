7ca582dd16fafd9a4bbe80b0a95b59fc5229e8c9
Dejan Milicic <dejan.milicic@gmail.com>
2021-03-16 12:26:29 +0100

    document expiration

---
+++ Northwind/Northwind/Features/Expiration/Expiration.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Northwind.Models.Entity;
+
+namespace Northwind.Features.Expiration
+{
+    public class Expiration
+    {
+        public void SetExpiration()
+        {
+            using var session = DocumentStoreHolder.Store.OpenSession();
+
+            var employee = session.Load<Employee>("employees/42-A");
+            session.Advanced.GetMetadataFor(employee)["@expires"] = DateTime.UtcNow.AddMinutes(1);
+            session.SaveChanges();
+        }
+    }
+}
