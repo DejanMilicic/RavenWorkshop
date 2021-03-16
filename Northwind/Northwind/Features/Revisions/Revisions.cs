c18a041f89dd03dc8e6844abed0bab6adfdcd20e
Dejan Milicic <dejan.milicic@gmail.com>
2021-03-16 09:05:40 +0100

    revisions

---
+++ Northwind/Northwind/Features/Revisions/Revisions.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Northwind.Models.Entity;
+
+namespace Northwind.Features.Revisions
+{
+    public class Revisions
+    {
+        public void ForceCreation()
+        {
+            using var session = DocumentStoreHolder.Store.OpenSession();
+
+            Employee employee = new Employee
+            {
+                FirstName = "Rev",
+                LastName = "Revision"
+            };
+
+            session.Store(employee);
+            session.SaveChanges();
+
+            session.Advanced.Revisions.ForceRevisionCreationFor(employee);
+            session.SaveChanges();
+
+            employee.FirstName = "Rev2";
+            session.SaveChanges();
+        }
+
+        public void GetRevisions()
+        {
+            using var session = DocumentStoreHolder.Store.OpenSession();
+
+            var revisions = session.Advanced.Revisions.GetFor<Employee>("employees/8-A");
+
+            foreach (Employee revision in revisions)
+            {
+                Console.WriteLine(revision);
+            }
+        }
+    }
+}
