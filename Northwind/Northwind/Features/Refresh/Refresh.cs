4db7d3371cbd6b0c920577fcea99012980d90490
Dejan Milicic <dejan.milicic@gmail.com>
2021-03-16 13:39:21 +0100

    refresh

---
+++ Northwind/Northwind/Features/Refresh/Refresh.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Northwind.Models.Entity;
+
+namespace Northwind.Features.Refresh
+{
+    public class Refresh
+    {
+        public void SetRefresh()
+        {
+            using var session = DocumentStoreHolder.Store.OpenSession();
+
+            var employee = session.Load<Employee>("employees/8-A");
+            session.Advanced.GetMetadataFor(employee)["@refresh"] = DateTime.UtcNow.AddMinutes(1);
+            session.SaveChanges();
+        }
+    }
+}
