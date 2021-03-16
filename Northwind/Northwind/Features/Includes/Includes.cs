77b4ff13232ce938b073fe6b0d73f2af42bfbcc5
Dejan Milicic <dejan.milicic@gmail.com>
2021-03-16 14:50:22 +0100

    includes

---
+++ Northwind/Northwind/Features/Includes/Includes.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Northwind.Models.Entity;
+using Raven.Client.Documents;
+
+namespace Northwind.Features.Includes
+{
+    public class Includes
+    {
+        public void ManyCalls()
+        {
+            using var session = DocumentStoreHolder.Store.OpenSession();
+
+            var orders = session.Query<Order>()
+                .Where(order => order.Company == "Companies/2-A")
+                .ToList();
+
+            foreach (Order order in orders)
+            {
+                var employee = session.Load<Employee>(order.Employee);
+                var company = session.Load<Company>(order.Company);
+                Console.WriteLine($"Order: {order.Id} \t {order.OrderedAt} \t via {employee.FirstName} \t for {company.Name}");
+            }
+
+            Console.WriteLine($"Total number of requests: {session.Advanced.NumberOfRequests}");
+        }
+
+        public void ManyCallsOptimized()
+        {
+            using var session = DocumentStoreHolder.Store.OpenSession();
+
+            var orders = session.Query<Order>()
+                .Include(x => x.Employee)
+                .Include(x => x.Company)
+                .Where(order => order.Company == "Companies/2-A")
+                .ToList();
+
+            foreach (Order order in orders)
+            {
+                var employee = session.Load<Employee>(order.Employee);
+                var company = session.Load<Company>(order.Company);
+                Console.WriteLine($"Order: {order.Id} \t {order.OrderedAt} \t via {employee.FirstName} \t for {company.Name}");
+            }
+
+            Console.WriteLine($"Total number of requests: {session.Advanced.NumberOfRequests}");
+        }
+    }
+}
