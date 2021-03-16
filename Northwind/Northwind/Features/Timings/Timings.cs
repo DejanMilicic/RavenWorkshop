d372a3c4cdb867030d5e7a78e4b725073213725b
Dejan Milicic <dejan.milicic@gmail.com>
2021-03-16 19:29:35 +0100

    timings

---
+++ Northwind/Northwind/Features/Timings/Timings.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Northwind.Models.Entity;
+using Raven.Client.Documents.Queries.Timings;
+
+namespace Northwind.Features.Timings
+{
+    public class Timings
+    {
+        public void GetTimings()
+        {
+            using var session = DocumentStoreHolder.Store.OpenSession();
+
+            var employees = session.Advanced.DocumentQuery<Employee>()
+                .Timings(out QueryTimings timings)
+                .WhereEquals(x => x.Address.Country, "UK")
+                .ToList();
+
+            Console.WriteLine($"{employees.Count} employees fetched in {timings.DurationInMs}ms");
+        }
+    }
+}
