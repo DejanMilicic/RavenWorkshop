dee5527463dad6ccfe125f34929f6a3712ad5250
Dejan Milicic <dejan.milicic@gmail.com>
2021-03-16 22:14:41 +0100

    Spatial query

---
+++ Northwind/Northwind/Features/Spatial/Spatial.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Northwind.Models.Entity;
+
+namespace Northwind.Features.Spatial
+{
+    public class Spatial
+    {
+        public void OrdersInParis()
+        {
+            using var session = DocumentStoreHolder.Store.OpenSession();
+
+            var orders = session.Advanced.RawQuery<Order>(@"
+                from index 'Orders/ByShipment/Location'
+                where spatial.within(ShipmentLocation, spatial.circle(10, 48.8566, 2.3522))
+            ").ToList();
+
+            foreach (Order order in orders)
+            {
+                Console.WriteLine($"{order.Id}");
+            }
+        }
+    }
+}
