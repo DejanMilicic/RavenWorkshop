13a76963f7cbf5f042b3703140ae1ec1a354de5a
Dejan Milicic <dejan.milicic@gmail.com>
2021-03-17 13:03:24 +0100

    timeseries

---
+++ Northwind/Northwind/Features/Timeseries/Timeseries.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Northwind.Models.Entity;
+
+namespace Northwind.Features.Timeseries
+{
+    public class Timeseries
+    {
+        public void Do()
+        {
+            using var bulk = DocumentStoreHolder.Store.BulkInsert();
+            var random = new Random();
+            var time = DateTime.Today.AddDays(-30);
+
+            for (int i = 1; i < 10; i++)
+            {
+                using var ts = bulk.TimeSeriesFor($"employees/{i}-A", "Audits");
+
+                for (int j = 0; j < 10_000; j++)
+                {
+                    time = time.AddHours(random.Next(6, 24));
+                    ts.Append(time, 1, "Login");
+                    
+                    time = time.AddSeconds(random.Next(600, 10_000));
+                    ts.Append(time, 0, "Logout");
+                }
+            }
+        }
+    }
+}
