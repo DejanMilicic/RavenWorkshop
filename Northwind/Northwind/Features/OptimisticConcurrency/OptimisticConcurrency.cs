02fb9c011f956d773efeaa7e1ae01e6081901b33
Dejan Milicic <dejan.milicic@gmail.com>
2021-03-17 10:57:31 +0100

    more patching

--- Northwind/Northwind/Features/OptimisticConcurrency/OptimisticConcurrency.cs
+++ Northwind/Northwind/Features/OptimisticConcurrency/OptimisticConcurrency.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Northwind.Models.Entity;
+using Raven.Client.Documents.Commands.Batches;
+using Raven.Client.Documents.Operations;
+using Raven.Client.Documents.Session;
 using Raven.Client.Exceptions;
 
 namespace Northwind.Features.OptimisticConcurrency
@@ -38,6 +41,50 @@ namespace Northwind.Features.OptimisticConcurrency
                 session.Advanced.UseOptimisticConcurrency = true;
                 var order = session.Load<Order>("orders/1-A");
                 order.Freight++; // breakpoint
+                session.SaveChanges();
+            }
+            catch (ConcurrencyException e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+        }
+
+        // use patch to update single property
+        public void UsePatch()
+        {
+            try
+            {
+                using var session = DocumentStoreHolder.Store.OpenSession();
+                session.Advanced.UseOptimisticConcurrency = true;
+                var order = session.Load<Order>("orders/1-A");
+                //order.Freight++; // breakpoint
+                session.Advanced.Patch(order, o => o.Freight, order.Freight+1);
+                session.SaveChanges();
+            }
+            catch (ConcurrencyException e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+        }
+
+        public void UseDeferredPatch()
+        {
+            try
+            {
+                using var session = DocumentStoreHolder.Store.OpenSession();
+                session.Advanced.UseOptimisticConcurrency = true;
+                var order = session.Load<Order>("orders/1-A");
+                //order.Freight++; // breakpoint
+                session.Advanced.Patch(order, o => o.Freight, order.Freight+1);
+
+                session.Advanced.Defer(new PatchCommandData("orders/1-A", null, 
+                    new PatchRequest
+                    {
+                        Script = "this.Freight++;"
+                    }, null));
+
                 session.SaveChanges();
             }
             catch (ConcurrencyException e)
