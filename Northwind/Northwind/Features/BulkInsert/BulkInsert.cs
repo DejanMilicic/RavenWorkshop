b9e4cf04a8a5c3ea57a941ee1987c4c345a846e8
Dejan Milicic <dejan.milicic@gmail.com>
2021-03-17 12:47:55 +0100

    bulk insert

---
+++ Northwind/Northwind/Features/BulkInsert/BulkInsert.cs
@@ -0,0 +1,21 @@
+﻿using Northwind.Models.Entity;
+
+namespace Northwind.Features.BulkInsert
+{
+    public class BulkInsert
+    {
+        public void Do()
+        {
+            using var bulk = DocumentStoreHolder.Store.BulkInsert();
+
+            for (int i = 0; i < 1000 * 1000; i++)
+            {
+                bulk.Store(new Employee
+                {
+                    FirstName = "FirstName #" + i,
+                    LastName = "LastName #" + i
+                });
+            }
+        }
+    }
+}
