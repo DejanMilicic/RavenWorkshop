3c64143df0b668226b877d541cce896ade5421fe
Dejan Milicic <dejan.milicic@gmail.com>
2021-03-17 08:27:54 +0100

    changes api

---
+++ Northwind/Northwind/Features/Changes/Changes.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Northwind.Features.Changes
+{
+    public class Changes
+    {
+        public void Consume()
+        {
+            DocumentStoreHolder.Store.Changes()
+                .ForAllDocuments()
+                .Subscribe(change =>
+                {
+                    Console.WriteLine($"{change.Id} {change.Type}");
+                });
+
+            Console.ReadLine();
+        }
+    }
+}
