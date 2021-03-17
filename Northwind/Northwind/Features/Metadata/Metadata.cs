79bae86f7099f5d272bc5c7288c96eef06c82c02
Dejan Milicic <dejan.milicic@gmail.com>
2021-03-17 13:43:11 +0100

    metadata

---
+++ Northwind/Northwind/Features/Metadata/Metadata.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Northwind.Features.Recursive;
+using Northwind.Models.Entity;
+
+namespace Northwind.Features.Metadata
+{
+    public class Metadata
+    {
+        public void Create()
+        {
+            using var session = DocumentStoreHolder.Store.OpenSession();
+            var emp = session.Load<Employee>("employees/8-A");
+
+            session.Advanced.GetMetadataFor(emp)["IsDeleted"] = "true";
+
+            session.SaveChanges();
+        }
+
+        public void Read()
+        {
+            using var session = DocumentStoreHolder.Store.OpenSession();
+            var emp = session.Load<Employee>("employees/8-A");
+
+            string isDeleted = (string)session.Advanced.GetMetadataFor(emp)["IsDeleted"];
+
+            Console.WriteLine($"{emp.FirstName}: {isDeleted}");
+        }
+    }
+}
