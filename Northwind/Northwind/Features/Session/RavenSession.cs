3f26ef55015b52394d9d302cacd0858498df6fb8
Dejan Milicic <dejan.milicic@gmail.com>
2021-04-21 13:02:43 +0200

    session evict example

---
+++ Northwind/Northwind/Features/Session/RavenSession.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Northwind.Models.Entity;
+using Raven.Client.Documents.Session;
+
+namespace Northwind.Features.Session
+{
+    public class RavenSession
+    {
+        public void Evict()
+        {
+            using var session = DocumentStoreHolder.Store.OpenSession();
+
+            Employee laura = session.Load<Employee>("employees/8-A");
+            
+            session.Advanced.Evict(laura);
+
+            laura = session.Load<Employee>("employees/8-A");
+            
+            Console.WriteLine($"Total requests: {session.Advanced.NumberOfRequests}");
+        }
+    }
+}
