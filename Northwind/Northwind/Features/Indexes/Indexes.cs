93c69aed87036122ee9e51360f82c03fbc683c4c
Dejan Milicic <dejan.milicic@gmail.com>
2021-06-29 00:41:01 +0200

    Get stale indexes

---
+++ Northwind/Northwind/Features/Indexes/Indexes.cs
@@ -0,0 +1,14 @@
+﻿using Raven.Client.Documents.Operations;
+
+namespace Northwind.Features.Indexes
+{
+    public class Indexes
+    {
+        public void GetStaleIndexes()
+        {
+            var stats = DocumentStoreHolder.Store.Maintenance.Send(new GetStatisticsOperation());
+
+            string[] staleIndexes = stats.StaleIndexes;
+        }
+    }
+}
