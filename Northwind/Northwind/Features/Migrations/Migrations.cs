81dc85747a6f68ef874bdb1ff3e85fd0b4853640
Dejan Milicic <dejan.milicic@gmail.com>
2021-08-05 14:42:43 +0200

    Migrations - missing property

---
+++ Northwind/Northwind/Features/Migrations/Migrations.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Northwind.Models.Entity;
+
+namespace Northwind.Features.Migrations
+{
+    public class Migrations
+    {
+        public void MissingProperty()
+        {
+            using var session = DocumentStoreHolder.Store.OpenSession();
+
+            Category seafood = session.Load<Category>("categories/8-A");
+            seafood.Name += " 2";
+            session.SaveChanges();
+            
+            Console.WriteLine($"Total requests: {session.Advanced.NumberOfRequests}");
+        }
+    }
+}
--- Northwind/Northwind/Models/Entity/Category.cs
+++ Northwind/Northwind/Models/Entity/Category.cs
@@ -4,6 +4,6 @@
     {
         public string Id { get; set; }
         public string Name { get; set; }
-        public string Description { get; set; }
+        //public string Description { get; set; }
     }
 }
