448861ad3beeb378831d5604973ae651b9485ce2
Dejan Milicic <dejan.milicic@gmail.com>
2021-08-20 00:24:58 +0200

    orders omnisearch

---
+++ Northwind/Northwind/Features/Search/Orders_Omnisearch.cs
@@ -0,0 +1,56 @@
+﻿using System.Linq;
+using Northwind.Models.Entity;
+using Raven.Client.Documents.Indexes;
+
+namespace Northwind.Features.Search
+{
+    public class Orders_Omnisearch : AbstractMultiMapIndexCreationTask<Orders_Omnisearch.Entry>
+    {
+        public class Entry
+        {
+            public string[] Query { get; set; }
+        }
+
+        public Orders_Omnisearch()
+        {
+            AddMap<Order>(orders => from order in orders
+                from orderLine in order.Lines
+
+                let product = LoadDocument<Product>(orderLine.Product)
+                let category = LoadDocument<Category>(product.Category)
+                let supplier = LoadDocument<Supplier>(product.Supplier)
+
+                select new Entry
+                {
+                    Query = new[]
+                    {
+                        orderLine.ProductName,
+                        product.QuantityPerUnit,
+                        category.Name,
+                        supplier.Name
+                    }
+                }
+            );
+
+            AddMap<Order>(orders => from order in orders
+
+                let company = LoadDocument<Company>(order.Company)
+                let employee = LoadDocument<Employee>(order.Employee)
+
+                select new Entry
+                {
+                    Query = new[]
+                    {
+                        company.Name, 
+                        company.Address.City,
+                        company.Address.Country,
+                        employee.FirstName,
+                        employee.LastName
+                    }
+                }
+            );
+
+            Index(x => x.Query, FieldIndexing.Search);
+        }
+    }
+}
--- Northwind/Northwind/Features/Search/Search.cs
+++ Northwind/Northwind/Features/Search/Search.cs
@@ -24,5 +24,36 @@ namespace Northwind.Features.Search
                 Console.WriteLine($"Order {order.Id} contains {term}");
             }
         }
+        public void OrdersOmniSearch()
+        {
+            using var session = DocumentStoreHolder.Store.OpenSession();
+
+            string term = "maison"; // supplier name
+
+            var res = session.Query<Orders_Omnisearch.Entry, Orders_Omnisearch>()
+                .Search(x => x.Query, term)
+                .OfType<Order>()
+                .ToList();
+
+            Console.WriteLine($"{res.Count} orders returned when searching for {term}");
+            
+            term = "apples"; // product name
+
+            res = session.Query<Orders_Omnisearch.Entry, Orders_Omnisearch>()
+                .Search(x => x.Query, term)
+                .OfType<Order>()
+                .ToList();
+
+            Console.WriteLine($"{res.Count} orders returned when searching for {term}");            
+            
+            term = "boxes"; // packaging
+
+            res = session.Query<Orders_Omnisearch.Entry, Orders_Omnisearch>()
+                .Search(x => x.Query, term)
+                .OfType<Order>()
+                .ToList();
+
+            Console.WriteLine($"{res.Count} orders returned when searching for {term}");
+        }
     }
 }
