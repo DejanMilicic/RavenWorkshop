4d19670c32d6b098924e8f21bc556301c4e10379
Dejan Milicic <dejan.milicic@gmail.com>
2021-11-04 09:43:24 +0100

    todo

--- Northwind/Northwind/Features/BulkInsert/BulkInsert.cs
+++ Northwind/Northwind/Features/BulkInsert/BulkInsert.cs
@@ -6,6 +6,9 @@ namespace Northwind.Features.BulkInsert
     {
         public void Do()
         {
+             // todo: introduce timer so we can see how long it lasts
+             // todo: comparison with inserting via session
+
             using var bulk = DocumentStoreHolder.Store.BulkInsert();
 
             for (int i = 0; i < 1000 * 1000; i++)
