e6ac61b5849c3d2daf038c23d4f8d82503b1b911
Dejan Milicic <dejan.milicic@gmail.com>
2021-11-15 18:33:44 +0100

    one more polymorphism example

---
+++ Northwind/Northwind/Features/Polymorphism/SpecialShipments_ByWeight2.cs
@@ -0,0 +1,39 @@
+﻿using System.Linq;
+using System.Net.Sockets;
+using Raven.Client.Documents.Indexes;
+using Raven.Client.Documents.Linq.Indexing;
+
+namespace Northwind.Features.Polymorphism
+{
+    public class SpecialShipments_ByWeight2 : 
+        AbstractMultiMapIndexCreationTask<SpecialShipment>
+    {
+        public class Entry
+        {
+            public double TotalWeight { get; set; }
+        }
+
+        public SpecialShipments_ByWeight2()
+        {
+            // crate
+            AddMap<SpecialShipment>(ss => 
+                from s in ss 
+                    where AsJson(s.Item)["$type"] == "Northwind.Features.Polymorphism.Crate, Northwind"
+                select new Entry
+                {
+                    TotalWeight = ((Crate)s.Item).Weight
+                }
+            );
+
+            // car
+            AddMap<SpecialShipment>(ss =>
+                from s in ss
+                where AsJson(s.Item)["$type"] == "Northwind.Features.Polymorphism.Car, Northwind"
+                select new Entry
+                {
+                    TotalWeight = 555
+                }
+            );
+        }
+    }
+}
