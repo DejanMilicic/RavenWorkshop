0c7418b521b8e009c4f2c73de9e5c7357b3763dc
Dejan Milicic <dejan.milicic@gmail.com>
2021-11-16 09:38:29 +0100

    namespace corrected

--- Northwind/Northwind/Features/Sorting/Candidates_ByEducation.cs
+++ Northwind/Northwind/Features/Sorting/Candidates_ByEducation.cs
@@ -1,13 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Linq.Expressions;
-using System.Runtime.InteropServices.ComTypes;
-using Northwind.Features.Sorting;
+﻿using System.Collections.Generic;
 using Raven.Client.Documents.Indexes;
-using Raven.Client.Documents.Linq.Indexing;
 
-namespace Northwind.Features.Polymorphism
+namespace Northwind.Features.Sorting
 {
     public class Candidates_ByEducation : AbstractJavaScriptIndexCreationTask
     {
