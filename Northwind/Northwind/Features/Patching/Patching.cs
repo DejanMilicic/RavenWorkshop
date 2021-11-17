0f915598be6a28f9f53a86573f76377ae17363b7
Dejan Milicic <dejan.milicic@gmail.com>
2021-11-17 19:39:18 +0100

    patching from revision

---
+++ Northwind/Northwind/Features/Patching/Patching.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Northwind.Models.Entity;
+
+namespace Northwind.Features.Patching
+{
+    public class Patching
+    {
+        public void PatchFromRevision()
+        {
+            string orderId = "orders/830-A";
+
+            using var session = DocumentStoreHolder.Store.OpenSession();
+            List<Order> revisions = session.Advanced.Revisions.GetFor<Order>(orderId).ToList();
+
+            var revisionDate = revisions.Skip(1).First().OrderedAt;
+            session.Advanced.Patch<Order, DateTime>(orderId, x => x.OrderedAt, revisionDate);
+            
+            session.SaveChanges();
+        }
+    }
+}
