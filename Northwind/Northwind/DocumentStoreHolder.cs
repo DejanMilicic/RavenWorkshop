4aaa6902170722bfdb909cf655f4cfe95b746047
Dejan Milicic <dejan.milicic@gmail.com>
2021-11-26 15:10:03 +0100

    refactoring

--- Northwind/Northwind/DocumentStoreHolder.cs
+++ Northwind/Northwind/DocumentStoreHolder.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Security.Cryptography.X509Certificates;
 using Raven.Client.Documents;
 using Raven.Client.Documents.Indexes;
 
@@ -6,14 +7,19 @@ namespace Northwind
 {
     public static class DocumentStoreHolder
     {
+        public static IDocumentStore GetStore()
+        {
+            return new DocumentStore
+            {
+                Urls = new[] { "http://127.0.0.1:8080" },
+                Database = "demo"
+            };
+        }
+
         private static readonly Lazy<IDocumentStore> LazyStore =
             new Lazy<IDocumentStore>(() =>
             {
-                IDocumentStore store = new DocumentStore
-                {
-                    Urls = new[] { "http://127.0.0.1:8080" },
-                    Database = "demo"
-                };
+                IDocumentStore store = GetStore();
 
                 store.Initialize();
 
