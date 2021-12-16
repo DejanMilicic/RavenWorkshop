af2a741584c4d8153da4608be6f31e4795c38040
Dejan Milicic <dejan.milicic@gmail.com>
2021-12-17 00:06:49 +0100

    Document session examples

--- Northwind/Northwind/Features/Caching/Caching.cs
+++ Northwind/Northwind/Features/Caching/Caching.cs
@@ -66,5 +66,41 @@ namespace Northwind.Features.Caching
                 Console.ReadLine();
             }
         }
+
+        public void DocumentSessionIdentityMap()
+        {
+            using var session = DocumentStoreHolder.Store.OpenSession();
+
+            session.Load<Employee>("employees/8-A");
+            session.Load<Employee>("employees/8-A");
+            Console.WriteLine($"Total number of requests: {session.Advanced.NumberOfRequests}");
+        }
+
+        public void DocumentSessionEvict()
+        {
+            using var session = DocumentStoreHolder.Store.OpenSession();
+
+            Employee emp = session.Load<Employee>("employees/8-A");
+            session.Advanced.Evict(emp);
+            session.Load<Employee>("employees/8-A");
+            Console.WriteLine($"Total number of requests: {session.Advanced.NumberOfRequests}");
+        }
+
+        public void DocumentSessionNotModified()
+        {
+            DocumentStore store = (DocumentStore)DocumentStoreHolder.GetStore();
+            store.OnSucceedRequest += (sender, e) => Console.WriteLine($"{e.Response.StatusCode}");
+            store.Initialize();
+
+            using (var s1 = store.OpenSession())
+            {
+                s1.Load<Employee>("employees/8-A");
+            }
+
+            using (var s2 = store.OpenSession())
+            {
+                s2.Load<Employee>("employees/8-A");
+            }
+        }
     }
 }
