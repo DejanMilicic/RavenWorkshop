614535719e07e207ec0e504b8013a07d78fae930
Dejan Milicic <dejan.milicic@gmail.com>
2021-12-17 12:19:34 +0100

    checking if index was stale

---
+++ Northwind/Northwind/Features/StaleIndex/StaleIndex.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Northwind.Features.Indexes;
+using Northwind.Models.Entity;
+using Raven.Client.Documents.Session;
+
+namespace Northwind.Features.StaleIndex
+{
+    public static class StaleIndex
+    {
+        public static void IsIndexStale()
+        {
+            using var session = DocumentStoreHolder.Store.OpenSession();
+
+            List<Order> orders = session.Query<Orders_ByCompany.Entry, Orders_ByCompany>()
+                .Statistics(out QueryStatistics stats)
+                .Where(x => x.CompanyName == "companyName")
+                .OfType<Order>()
+                .ToList();
+
+            Console.WriteLine($"Stale index: {stats.IsStale}");
+        }
+    }
+}
