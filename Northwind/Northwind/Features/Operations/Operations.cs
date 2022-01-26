ff845bc35970d4257920298ec6b89a222ea85c4c
Dejan Milicic <dejan.milicic@gmail.com>
2022-01-26 11:45:33 +0100

    create database operation

---
+++ Northwind/Northwind/Features/Operations/Operations.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Raven.Client.ServerWide;
+using Raven.Client.ServerWide.Operations;
+
+namespace Northwind.Features.Operations
+{
+    public class Operations
+    {
+        public static void CreateDatabase()
+        {
+            var store = DocumentStoreHolder.Store;
+
+            store.Maintenance.Server.Send(new CreateDatabaseOperation(new DatabaseRecord("sample_" + Guid.NewGuid())));
+        }
+    }
+}
