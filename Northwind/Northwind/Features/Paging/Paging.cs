c8306ceef4cbf69e5ebb96b037a301b654b9611d
Dejan Milicic <dejan.milicic@gmail.com>
2022-02-10 12:13:46 +0100

    show generated RQL

--- Northwind/Northwind/Features/Paging/Paging.cs
+++ Northwind/Northwind/Features/Paging/Paging.cs
@@ -6,14 +6,24 @@ using Raven.Client.Documents.Session;
 
 namespace Northwind.Features.Paging
 {
-    public class Paging
+    public static class Paging
     {
-        public void FetchPage()
+        public static void FetchPage()
         {
             using var session = DocumentStoreHolder.Store.OpenSession();
 
             int page = 1;
             int pageSize = 10;
+
+
+            var rql = session.Query<Order>()
+                .Statistics(out QueryStatistics s1) // statistics are optional
+                .Skip(page * pageSize)
+                .Take(pageSize)
+                .ToString();
+
+            Console.WriteLine(rql);
+            Console.WriteLine();
 
             var pagedResults = session.Query<Order>()
                 .Statistics(out QueryStatistics stats) // statistics are optional
