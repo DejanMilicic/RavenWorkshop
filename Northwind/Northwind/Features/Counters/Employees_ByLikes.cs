bbddc3c86aebf0f3f52bde833d37cf522993fd6b
Dejan Milicic <dejan.milicic@gmail.com>
2022-02-10 19:35:15 +0100

    name corrected

---
+++ Northwind/Northwind/Features/Counters/Employees_ByLikes.cs
@@ -0,0 +1,20 @@
+﻿using System.Linq;
+using Northwind.Models.Entity;
+using Raven.Client.Documents.Indexes.Counters;
+
+namespace Northwind.Features.Counters
+{
+    public class Employees_ByLikes : AbstractCountersIndexCreationTask<Employee>
+    {
+        public Employees_ByLikes()
+        {
+            AddMap("Likes", counters => from counter in counters
+                select new
+                {
+                    Likes = counter.Value,
+                    Name = counter.Name,
+                    Employee = counter.DocumentId
+                });
+        }
+    }
+}
