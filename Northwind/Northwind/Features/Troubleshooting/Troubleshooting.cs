ffd76c1a40a958332c1ceffd38edf17eaf090fa7
Dejan Milicic <dejan.milicic@gmail.com>
2022-02-10 12:09:17 +0100

    Gettting request size

---
+++ Northwind/Northwind/Features/Troubleshooting/Troubleshooting.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Northwind.Models.Entity;
+
+namespace Northwind.Features.Troubleshooting
+{
+    public static class Troubleshooting
+    {
+        public static void RequestSize()
+        {
+            using var session = DocumentStoreHolder.Store.OpenSession();
+
+            Employee emp = session.Load<Employee>("employees/8-A");
+            emp.FirstName = Guid.NewGuid().ToString();
+
+            session.Advanced.DocumentStore.GetRequestExecutor().OnBeforeRequest += (s, e) =>
+            {
+                Console.WriteLine($"Request size: {e.Request.Content?.ReadAsStringAsync().Result.Length ?? 0}b");
+            };
+
+            session.SaveChanges();
+        }
+    }
+}
