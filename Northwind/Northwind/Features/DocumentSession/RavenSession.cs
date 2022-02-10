476147ca6229b94a6f0b2ebc00370ad0a9834eb9
Dejan Milicic <dejan.milicic@gmail.com>
2022-02-10 12:44:03 +0100

    RavenSession renamed to DocumentSession

---
+++ Northwind/Northwind/Features/DocumentSession/RavenSession.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Northwind.Models.Entity;
+using Raven.Client.Documents.Session;
+
+namespace Northwind.Features.Session
+{
+    public class RavenSession
+    {
+        public void Evict()
+        {
+            using var session = DocumentStoreHolder.Store.OpenSession();
+
+            Employee laura = session.Load<Employee>("employees/8-A");
+            
+            session.Advanced.Evict(laura);
+
+            laura = session.Load<Employee>("employees/8-A");
+            
+            Console.WriteLine($"Total requests: {session.Advanced.NumberOfRequests}");
+        }
+
+        public static void WhatChanged()
+        {
+            using var session = DocumentStoreHolder.Store.OpenSession();
+
+            Employee laura = session.Load<Employee>("employees/8-A");
+            laura.FirstName = Guid.NewGuid().ToString();
+            laura.LastName = Guid.NewGuid().ToString();
+
+            Employee newEmployee = new Employee();
+            newEmployee.FirstName = "Marco";
+            session.Store(newEmployee);
+
+            IDictionary<string, DocumentsChanges[]> whatChanged = session.Advanced.WhatChanged();
+            int countBeforeSave = whatChanged.Count;
+            Console.WriteLine($"What changed before save changes: {countBeforeSave}");
+
+            session.SaveChanges();
+
+            int countAfterSave = session.Advanced.WhatChanged().Count;
+            Console.WriteLine($"What changed after save changes: {countAfterSave}");
+        }
+    }
+}
