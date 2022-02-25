f8b4009d32a6baace21a2709cfc544dc95df20e7
Dejan Milicic <dejan.milicic@gmail.com>
2022-02-25 10:20:42 +0100

    stream json document

---
+++ Northwind/Northwind/Features/Streaming/Streaming.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Northwind.Models.Entity;
+using Raven.Client.Documents;
+
+namespace Northwind.Features.Streaming
+{
+    public static class Streaming
+    {
+        public static void ToStream()
+        {
+            using (var stream = new MemoryStream())
+            using (var session = DocumentStoreHolder.Store.OpenSession())
+            {
+                session.Query<Employee>()
+                    .Where(x => x.FirstName == "Robert")
+                    .ToStream(stream);
+
+                stream.Position = 0;
+                var json = JObject.Load(new JsonTextReader(new StreamReader(stream)));
+                var res = json.GetValue("Results");
+
+                Console.WriteLine(res);
+            }
+        }
+    }
+}
