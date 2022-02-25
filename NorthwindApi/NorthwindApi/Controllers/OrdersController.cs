6a64c006c8edbeb30a2b087f5e1bc6b526f6aa4e
Dejan Milicic <dejan.milicic@gmail.com>
2022-02-25 12:56:40 +0100

    streaming query result

---
+++ NorthwindApi/NorthwindApi/Controllers/OrdersController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NorthwindApi.Infra;
+using NorthwindApi.Models.Entity;
+using Raven.Client.Documents;
+using Raven.Client.Documents.Session;
+
+namespace NorthwindApi.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class OrdersController : ControllerBase
+{
+    private readonly IDocumentStore _store;
+
+    public OrdersController(IDocumentStore store)
+    {
+        _store = store;
+    }
+
+    [HttpGet]
+    public AsyncQueryResult<Order> Get()
+    {
+        IAsyncDocumentSession session = _store.OpenAsyncSession();
+
+        var query = session.Advanced.AsyncDocumentQuery<Order>(collectionName: "Orders");
+
+        return new AsyncQueryResult<Order>(session, query);
+    }
+}
---
+++ NorthwindApi/NorthwindApi/Infra/AsyncQueryResult.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Raven.Client.Documents;
+using Raven.Client.Documents.Session;
+
+namespace NorthwindApi.Infra;
+
+public class AsyncQueryResult<T> : IActionResult
+{
+    private readonly IAsyncDocumentSession _session;
+    private readonly IAsyncDocumentQuery<T> _query;
+
+    public AsyncQueryResult(IAsyncDocumentSession session, IAsyncDocumentQuery<T> query)
+    {
+        _session = session;
+        _query = query;
+    }
+
+    public AsyncQueryResult(IAsyncDocumentSession session, IQueryable<T> query)
+        : this(session, query.ToAsyncDocumentQuery())
+    {
+
+    }
+
+    public async Task ExecuteResultAsync(ActionContext context)
+    {
+        using (_session)
+        {
+            var response = context.HttpContext.Response;
+            response.ContentType = "application/json";
+            await _query.ToStreamAsync(response.Body, context.HttpContext.RequestAborted);
+        }
+    }
+}
