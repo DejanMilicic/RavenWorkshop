3c4359751d658459918c1f241e8b4710db5453b2
Dejan Milicic <dejan.milicic@gmail.com>
2022-03-11 11:11:08 +0100

    create in-memory database

---
+++ Northwind/Northwind/Features/RavenDocumentStore/RavenDocumentStore.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using Raven.Client.Documents;
+using Raven.Client.ServerWide;
+using Raven.Client.ServerWide.Operations;
+
+namespace Northwind.Features.RavenDocumentStore
+{
+    public static class RavenDocumentStore
+    {
+        public static IDocumentStore GetStore()
+        {
+            return new DocumentStore
+            {
+                Urls = new[] { "http://127.0.0.1:8080" }
+            }.Initialize();
+        }
+
+        public static void CreateInMemoryDatabase()
+        {
+            GetStore().Maintenance.Server.Send(
+                new CreateDatabaseOperation(
+                    new DatabaseRecord("MyTestDatabase") 
+                        { Settings = new Dictionary<string, string>{ { "RunInMemory", "true" } }}
+                    ));
+        }
+    }
+}
