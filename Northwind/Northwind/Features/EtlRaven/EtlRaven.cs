1ccbffe3d63fe5ea0302fc1555fa082154a12bb1
Dejan Milicic <dejan.milicic@gmail.com>
2022-03-16 23:36:40 +0100

    transformation script added

--- Northwind/Northwind/Features/EtlRaven/EtlRaven.cs
+++ Northwind/Northwind/Features/EtlRaven/EtlRaven.cs
@@ -8,7 +8,7 @@ namespace Northwind.Features.EtlRaven
     // RavenDB ETL: https://ravendb.net/docs/article-page/4.2/csharp/server/ongoing-tasks/etl/raven
     public static class EtlRaven
     {
-        public static void EmployeesWithoutTransformationScript()
+        public static void EmployeesAndOrders()
         {
             IDocumentStore source = new DocumentStore
             {
@@ -32,8 +32,21 @@ namespace Northwind.Features.EtlRaven
                 {
                     new Transformation()
                     {
-                        Name = "loadAll",
+                        Name = "loadAllEmployees",
                         Collections = {"Employees"}
+                    },
+                    {
+                        new Transformation()
+                        {
+                            Name = "TransformOrders",
+                            Collections = {"Orders"},
+                            Script = @"
+var c = load(this.Company);
+this.CompanyName = c.Name;
+delete(this.Company);
+loadToOrders(this);
+"
+                        }
                     }
                 }
             };
