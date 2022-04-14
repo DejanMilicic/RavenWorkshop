ed04a1bf0e0071608ec5acb1bb327475aedaa70b
Dejan Milicic <dejan.milicic@gmail.com>
2022-04-14 14:27:27 +0200

    filtering

---
+++ Northwind/Northwind/Features/Filter/Filter.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Northwind.Models.Entity;
+using Raven.Client.Documents;
+using Raven.Client.Documents.Session;
+
+namespace Northwind.Features.Filter
+{
+    public static class Filter
+    {
+        public static void WithAutomaticIndex()
+        {
+            using var session = DocumentStoreHolder.Store.OpenSession();
+
+            List<Employee> emp = session.Query<Employee>()
+                .Where(x => x.FirstName == "Nancy")
+                .ToList();
+
+            foreach (Employee employee in emp)
+            {
+                Console.WriteLine($"{employee.FirstName} {employee.LastName}");
+            }
+        }
+
+        public static void WithoutAutomaticIndex()
+        {
+            using var session = DocumentStoreHolder.Store.OpenSession();
+
+            List<Employee> emp = session.Query<Employee>()
+                .Filter(x => x.FirstName == "Nancy")
+                .Statistics(out QueryStatistics stats)
+                .ToList();
+
+            foreach (Employee employee in emp)
+            {
+                Console.WriteLine($"{employee.FirstName} {employee.LastName}");
+            }
+
+            Console.WriteLine($"Scanned results: {stats.ScannedResults}");
+
+        }
+    }
+}
