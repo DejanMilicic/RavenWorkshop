35758b34e15ad6b4a649ae0498fa7765e3fd4b4b
Dejan Milicic <dejan.milicic@gmail.com>
2022-09-26 18:41:54 +0200

    code cleanup

--- Northwind/Northwind/Features/Facets2/Facet2Queries.cs
+++ Northwind/Northwind/Features/Facets2/Facet2Queries.cs
@@ -22,39 +22,24 @@ namespace Northwind.Features.Facets2
             Console.WriteLine();
         }
 
-        private static void PrintProducts(List<Product> products)
-        {
-            Console.WriteLine("Products: ({0})", products.Count);
-            //foreach (var product in products)
-            //{
-                
-            //}
-        }
-
         internal static void Execute()
         {
             var productsQueries = new ProductsQueries();
 
-            Dictionary<string, IList<string>> filters;
-            bool? availability;
-
-            availability = null;
-            filters = new Dictionary<string, IList<string>>();
             Console.WriteLine("Complete catalog");
-            PrintProducts(productsQueries.GetProducts(availability, filters));
-            PrintFacetResult(productsQueries.GetProductsFacets(availability, filters));
+            PrintFacetResult(productsQueries.GetProductsFacets(
+                available: null, 
+                filters: new Dictionary<string, IList<string>>()));
 
-            availability = null;
-            filters = new Dictionary<string, IList<string>> { { "Brand", new List<string> { "Nike" } } };
             Console.WriteLine("Catalog filtering: Brand='Nike'");
-            PrintProducts(productsQueries.GetProducts(availability, filters));
-            PrintFacetResult(productsQueries.GetProductsFacets(availability, filters));
+            PrintFacetResult(productsQueries.GetProductsFacets(
+                available: null, 
+                filters: new Dictionary<string, IList<string>> { { "Brand", new List<string> { "Nike" } } }));
 
-            availability = true;
-            filters = new Dictionary<string, IList<string>> { { "Size", new List<string> { "9" } } };
             Console.WriteLine("Catalog filtering: Availability=true AND Size='9'");
-            PrintProducts(productsQueries.GetProducts(availability, filters));
-            PrintFacetResult(productsQueries.GetProductsFacets(availability, filters));
+            PrintFacetResult(productsQueries.GetProductsFacets(
+                available: true,
+                filters: new Dictionary<string, IList<string>> { { "Size", new List<string> { "9" } } }));
         }
     }
 }
