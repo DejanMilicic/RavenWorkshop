02cc4b14e58db282c9cbfc9db2571d1ff80a87f9
Dejan Milicic <dejan.milicic@gmail.com>
2022-09-26 13:51:15 +0200

    Products/ByCategoryAndPrice

---
+++ Northwind/Northwind/Features/Facets/Products_ByCategoryAndPrice.cs
@@ -0,0 +1,26 @@
+﻿using System.Linq;
+using Northwind.Models.Entity;
+using Raven.Client.Documents.Indexes;
+
+namespace Northwind.Features.Facets
+{
+    internal class Products_ByCategoryAndPrice : AbstractIndexCreationTask<Product, Products_ByCategoryAndPrice.Entry>
+    {
+        public class Entry
+        {
+            public string CategoryName { get; set; }
+
+            public decimal PricePerUnit { get; set; }
+        }
+
+        public Products_ByCategoryAndPrice()
+        {
+            Map = products => from product in products
+                              select new Entry
+                              {
+                                  CategoryName = LoadDocument<Category>(product.Category).Name,
+                                  PricePerUnit = product.PricePerUnit
+                              };
+        }
+    }
+}
