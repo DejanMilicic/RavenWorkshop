f29fbc47f23cde83117e1577d50ad449ce41e870
Dejan Milicic <dejan.milicic@gmail.com>
2022-10-03 19:21:44 +0200

    definition of a room

--- Hotel2/Hotel/Hotel/Models/Room.cs
+++ Hotel2/Hotel/Hotel/Models/Room.cs
@@ -3,5 +3,13 @@
     internal class Room
     {
         public string Id { get; set; }
+
+        public string Number { get; set; }
+
+        public int NumberOfBeds { get; set; } // 1, 2, 3
+
+        public string Type { get; set; } // family, studio, suite, apartment
+
+        public bool InUse { get; set; }
     }
 }
