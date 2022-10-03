a9b5869da6a20e035de51625a84377a0b3835f1b
Dejan Milicic <dejan.milicic@gmail.com>
2022-10-03 18:10:17 +0200

    cleamup

--- Northwind/Northwind/Features/CRUD/Load.cs
+++ Northwind/Northwind/Features/CRUD/Load.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 using Northwind.Models.Entity;
 
 namespace Northwind.Features.CRUD
@@ -44,7 +40,5 @@ namespace Northwind.Features.CRUD
             // <"noneisting/id", null>
             // <"employees/8-a", object>
         }
-
-
     }
 }
