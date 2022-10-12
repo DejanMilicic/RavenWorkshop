89758c9578cbf84578c4a072dd5ce15746874bc1
Dejan Milicic <dejan.milicic@gmail.com>
2022-10-12 17:15:15 +0200

    code cleanup

--- Northwind/Northwind/Features/IndexingRelationships/Graph/GraphHelper.cs
+++ Northwind/Northwind/Features/IndexingRelationships/Graph/GraphHelper.cs
@@ -24,15 +24,7 @@ namespace Northwind.Features.IndexingRelationships.Graph
         {
             var res = new List<Entry>();
 
-
-            var scope = Raven.Server.Documents.Indexes.Static.CurrentIndexingScope.Current;
-            //dynamic document = (Raven.Server.Documents.Indexes.Static.DynamicArray)scope.LoadDocument(null, "1", "Numbers");
-            //var document = scope.LoadDocument(null, number.Id, "Numbers");
-            //var cd = (IEnumerable<KeyValuePair<object, object>>)document;
-            //List<KeyValuePair<object, object>> list = cd.ToList();
-            //var el = list.Skip(1).Take(1).Single();
-
-            //var xx = list.SingleOrDefault(x => x.Key == "folowedby");
+            CurrentIndexingScope scope = CurrentIndexingScope.Current;
 
             dynamic doc = scope.LoadDocument(null, number.Id, "Numbers");
 
@@ -52,31 +44,6 @@ namespace Northwind.Features.IndexingRelationships.Graph
                     );
                 }
             }
-
-            return res;
-
-
-            //string yy = xx.Value.ToString() ?? ":(";
-
-
-            //var array = ((DynamicArray)el.Value);
-            //if (array.Any())
-            //{
-            //    //var a = ((DynamicArray)el.Value)[0];
-
-            //    foreach (object num in ((DynamicArray)el.Value))
-            //    {
-            //        res.Add(
-            //            new Entry
-            //            {
-            //                Ancestor = number.Id,
-            //                //Distance = (((IEnumerable<object>)xx.Value).ToList())[0].ToString(),
-            //                Distance = "yy",
-            //                Descendant = num.ToString()
-            //            }
-            //            );
-            //    }
-            //}
 
             return res;
         }
