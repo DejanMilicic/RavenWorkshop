cc77e4da2442a6598f268a6b7691c07b80382e38
Dejan Milicic <dejan.milicic@gmail.com>
2022-11-02 11:14:16 +0100

    records examples

---
+++ Northwind/Northwind/Features/Records/Receipt.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using CSharpVitamins;
+
+namespace Northwind.Features.Records
+{
+    public record Receipt
+    {
+        public string Id { get; init; } = ShortGuid.Encode(Guid.NewGuid());
+
+        public string Store { get; init; }
+
+        public string Description { get; init; }
+
+        public decimal Amount { get; init; }
+
+        public DateTime PurchaseDate { get; init; }
+    }
+}
---
+++ Northwind/Northwind/Features/Records/Records.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Northwind.Features.Records
+{
+    public static class Records
+    {
+        public static void Create()
+        {
+            using var session = DocumentStoreHolder.Store.OpenSession();
+
+            Receipt receipt = new Receipt
+            {
+                Store = "My local store",
+                Description = "Shoes",
+                Amount = 199.99m,
+                PurchaseDate = DateTime.UtcNow
+            };
+
+            session.Store(receipt);
+            session.SaveChanges();
+        }
+
+        public static void Update()
+        {
+            using var session = DocumentStoreHolder.Store.OpenSession();
+
+            Receipt receipt = session.Query<Receipt>().First();
+
+            session.Advanced.Evict(receipt);
+            session.Store(receipt with { Store = "Big store" });
+            
+            session.SaveChanges();
+        }
+    }
+}
