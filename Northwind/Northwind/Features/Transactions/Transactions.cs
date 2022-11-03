fd57efb4a5387f7b35a4468b47a34866d4247f7c
Dejan Milicic <dejan.milicic@gmail.com>
2022-11-03 11:13:53 +0100

    transactions

---
+++ Northwind/Northwind/Features/Transactions/Transactions.cs
@@ -0,0 +1,43 @@
+﻿using Northwind.Models.Entity;
+using Raven.Client.Documents.Session;
+
+namespace Northwind.Features.Transactions
+{
+    public class Transactions
+    {
+        public void LocalNodeTransaction()
+        {
+            using var session = DocumentStoreHolder.Store.OpenSession();
+
+            Employee emp = new Employee { FirstName = "Jane", LastName = "Doe" };
+
+            session.Store(emp);
+            session.SaveChanges();
+        }
+
+        public void ClusterWideTransaction()
+        {
+            using var session = DocumentStoreHolder.Store.OpenSession(
+                new SessionOptions { TransactionMode = TransactionMode.ClusterWide });
+
+            Employee emp = new Employee { FirstName = "Jane", LastName = "Doe" };
+
+            session.Store(emp);
+            session.SaveChanges();
+        }
+
+        public void WaitForReplication()
+        {
+            using var session = DocumentStoreHolder.Store.OpenSession();
+
+            Employee emp = new Employee { FirstName = "Jane", LastName = "Doe" };
+
+            // SaveChanges() should wait for two nodes to confirm
+            // they accepted changes before returning
+            session.Advanced.WaitForReplicationAfterSaveChanges(replicas: 2);
+
+            session.Store(emp);
+            session.SaveChanges();
+        }
+    }
+}
