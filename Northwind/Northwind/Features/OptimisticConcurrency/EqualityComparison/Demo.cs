90b5ab069cad676a3cdc487cf03e1b094a7bd182
Dejan Milicic <dejan.milicic@gmail.com>
2023-01-04 18:10:57 +0100

    check if strcturally equal entity is being already tracked by the session and prevent overriding

---
+++ Northwind/Northwind/Features/OptimisticConcurrency/EqualityComparison/Demo.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+using Raven.Client.Documents;
+
+namespace Northwind.Features.OptimisticConcurrency.EqualityComparison
+{
+    public static class EqualityComparison
+    {
+        public static void Demo()
+        {
+            var store = new DocumentStore
+            {
+                Urls = new[] { "http://127.0.0.1:8080" },
+                Database = "Demo"
+            }.Initialize();
+
+            Patient pa = new Patient
+            {
+                Id = Guid.NewGuid().ToString(),
+                Name = "John",
+                Description = "original desc"
+            };
+
+            Patient pb = new Patient
+            {
+                Id = Guid.NewGuid().ToString(),
+                Name = "John",
+                Description = "edited desc"
+            };
+
+            using var session = store.OpenSession();
+            session.Store(pa);
+
+            var exists = session.Advanced
+                .GetTrackedEntities()
+                .Select(x => x.Value)
+                .Any(x => x.Entity is Patient patient && patient.Name == pb.Name);
+
+            if (!exists)
+                session.Store(pb);
+
+            session.SaveChanges();
+        }
+    }
+}
---
+++ Northwind/Northwind/Features/OptimisticConcurrency/EqualityComparison/Model.cs
@@ -0,0 +1,12 @@
+﻿
+namespace Northwind.Features.OptimisticConcurrency.EqualityComparison
+{
+    public class Patient
+    {
+        public string Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+    }
+}
