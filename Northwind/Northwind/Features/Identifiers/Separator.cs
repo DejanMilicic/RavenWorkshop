96e01ab04d7201e45f6c3cb86db761cd8c8c358a
Dejan Milicic <dejan.milicic@gmail.com>
2023-01-16 21:22:50 +0100

    custom separator demo

---
+++ Northwind/Northwind/Features/Identifiers/Separator.cs
@@ -0,0 +1,35 @@
+﻿using Northwind.Models.Entity;
+using Raven.Client.Documents;
+
+namespace Northwind.Features.Identifiers
+{
+    public static class Separator
+    {
+        private static IDocumentStore GetStore()
+        {
+            var store = (new DocumentStore
+            {
+                Urls = new[] { "http://127.0.0.1:8080" },
+                Database = "Demo"
+            });
+
+            store.Conventions.IdentityPartsSeparator = '-';
+
+            store.Initialize();
+
+            return store;
+        }
+
+        public static void Demo()
+        {
+            Employee emp = new Employee();
+            emp.FirstName = "Seb";
+            emp.LastName = "Separator";
+
+            using var session = GetStore().OpenSession();
+
+            session.Store(emp);
+            session.SaveChanges();
+        }
+    }
+}
