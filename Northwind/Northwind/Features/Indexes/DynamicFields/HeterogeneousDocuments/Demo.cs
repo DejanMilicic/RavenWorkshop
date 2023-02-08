d6d9756b32f7e97a3ff8e4db8dd0f94e56dd49de
Dejan Milicic <dejan.milicic@gmail.com>
2023-02-09 00:04:59 +0100

    indexing heterogenous documents

---
+++ Northwind/Northwind/Features/Indexes/DynamicFields/HeterogeneousDocuments/Demo.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using Raven.Client.Documents;
+using Raven.Client.Documents.Indexes;
+using Spectre.Console;
+
+namespace Northwind.Features.Indexes.DynamicFields.HeterogeneousDocuments
+{
+    public static class HeterogeneousDocumentsIndexing
+    {
+        private static IDocumentStore GetStore()
+        {
+            var store = (new DocumentStore
+            {
+                Urls = new[] { "http://127.0.0.1:8080" },
+                Database = "demo"
+            }).Initialize();
+
+            IndexCreation.CreateIndexes(new AbstractIndexCreationTask[] { new Documents_ByAnyField() }, store);
+
+            return store;
+        }
+
+        public static void Demo()
+        {
+            using var session = GetStore().OpenSession();
+
+            List<dynamic> matchingDocuments = session
+                .Advanced
+                .DocumentQuery<object, Documents_ByAnyField>()
+                .WhereEquals("Name", "Western")
+                .OrElse()
+                .WhereEquals("Name", "B's Beverages")
+                .ToList();
+
+            AnsiConsole.Markup($"\n[black on yellow]Results[/]\n\n");
+            foreach (dynamic doc in matchingDocuments)
+                AnsiConsole.WriteLine(doc.Id.ToString());
+        }
+    }
+}
---
+++ Northwind/Northwind/Features/Indexes/DynamicFields/HeterogeneousDocuments/Documents_ByAnyField.cs
@@ -0,0 +1,23 @@
+﻿using Raven.Client.Documents.Indexes;
+using System.Collections.Generic;
+
+namespace Northwind.Features.Indexes.DynamicFields.HeterogeneousDocuments
+{
+    public class Documents_ByAnyField : AbstractJavaScriptIndexCreationTask
+    {
+        public Documents_ByAnyField()
+        {
+            // This will index EVERY FIELD under the top level of the document
+            Maps = new HashSet<string>
+            {
+                @"
+                map('@all_docs', function (d) {
+                    return {
+                        _: Object.keys(d).map(key => createField(key, d[key],
+                          { indexing: 'Search', storage: true, termVector: null }))
+                    }
+                })"
+            };
+        }
+    }
+}
