217278688fd0b0a5ee94854684613ca745ea939d
Dejan Milicic <dejan.milicic@gmail.com>
2023-02-10 17:45:05 +0100

    refactoring

--- Northwind/Northwind/Features/Indexes/IndexingAttachments/Demo.cs
+++ Northwind/Northwind/Features/Indexes/IndexingAttachments/Demo.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Linq;
 using Raven.Client.Documents;
 using Raven.Client.Documents.Indexes;
+using Raven.Client.Documents.Session;
 
 namespace Northwind.Features.Indexes.IndexingAttachments
 {
@@ -61,23 +62,38 @@ namespace Northwind.Features.Indexes.IndexingAttachments
             session.SaveChanges();
         }
 
+        public static List<Book> SearchBooks(IDocumentSession session, string term)
+        {
+            return session.Query<Library_Search.Entry, Library_Search>()
+                .Search(x => x.Content, term)
+                .OfType<Book>()
+                .ToList();
+        }
+
+        public static void PrintBookSearchResults(List<Book> books, string term)
+        {
+            Console.WriteLine($"Search results for: {term}");
+            Console.WriteLine("-");
+            foreach (Book book in books)
+            {
+                Console.WriteLine($"{book.Id} : {book.Title}");
+            }
+        }
+
         public static void Search()
         {
             using var session = GetStore().OpenSession();
 
             string term = "love";
+            var books = SearchBooks(session, term);
+            PrintBookSearchResults(books, term);
 
-            List<Book> books = session.Query<Library_Search.Entry, Library_Search>()
-                .Search(x => x.Content, term)
-                .OfType<Book>()
-                .ToList();
+            Console.WriteLine();
 
-            Console.WriteLine($"Search results for: {term}");
-            Console.WriteLine("===");
-            foreach (Book book in books)
-            {
-                Console.WriteLine($"{book.Id} : {book.Title}");
-            }
+            term = "jaw";
+            books = SearchBooks(session, term);
+            PrintBookSearchResults(books, term);
+
         }
     }
 }
