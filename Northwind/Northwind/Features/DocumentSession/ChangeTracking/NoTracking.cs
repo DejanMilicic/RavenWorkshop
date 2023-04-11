37b5b7eb8a70edbd8b746d477b484a09387578b7
Dejan Milicic <dejan.milicic@gmail.com>
2023-04-11 16:59:52 +0200

    refactoring

---
+++ Northwind/Northwind/Features/DocumentSession/ChangeTracking/NoTracking.cs
@@ -0,0 +1,28 @@
+﻿using Northwind.Models.Entity;
+using Raven.Client.Documents.Session;
+using System;
+
+namespace Northwind.Features.DocumentSession.ChangeTracking;
+
+public static class NoTracking
+{
+    public static void Demo()
+    {
+        using var session = DocumentStoreHolder.Store.OpenSession(new SessionOptions
+        {
+            NoTracking = true
+        });
+
+        session.Load<Employee>("employees/8-A");
+        session.Load<Employee>("employees/7-A");
+
+        // since session is not tracking loaded entities
+        // when we load them again, they will not be present in Identity Map
+        // and two more round trips will be made to the server
+        session.Load<Employee>("employees/8-A");
+        session.Load<Employee>("employees/7-A");
+
+        // so total number of requests sent will be 4
+        Console.WriteLine($"Total requests: {session.Advanced.NumberOfRequests}");
+    }
+}
--- Northwind/Northwind/Features/DocumentSession/DocumentSession.cs
+++ Northwind/Northwind/Features/DocumentSession/DocumentSession.cs
@@ -61,22 +61,6 @@ namespace Northwind.Features.DocumentSession
             Console.WriteLine($"Total requests: {session.Advanced.NumberOfRequests}");
         }
 
-        public static void NoTracking()
-        {
-            using var session = DocumentStoreHolder.Store.OpenSession(new SessionOptions
-            {
-                NoTracking = true
-            });
-            
-            Employee laura = session.Load<Employee>("employees/8-A");
-            Employee robert = session.Load<Employee>("employees/7-A");
-            
-            laura = session.Load<Employee>("employees/8-A");
-            robert = session.Load<Employee>("employees/7-A");
-            
-            Console.WriteLine($"Total requests: {session.Advanced.NumberOfRequests}");
-        }
-
         public static void SelectiveTracking()
         {
             var store = (DocumentStore)DocumentStoreHolder.GetStore();
