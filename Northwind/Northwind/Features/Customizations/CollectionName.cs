ce3ffb0ae4ea5fa12fc6cd05687f8d26708651eb
Dejan Milicic <dejan.milicic@gmail.com>
2023-05-05 09:52:14 +0200

    changing collection name convention

---
+++ Northwind/Northwind/Features/Customizations/CollectionName.cs
@@ -0,0 +1,42 @@
+﻿using Northwind.Models.Entity;
+using Raven.Client.Documents;
+using Raven.Client.Documents.Conventions;
+
+namespace Northwind.Features.Customizations;
+
+public static class CollectionName
+{
+    public static void Convention()
+    {
+        IDocumentStore store = new DocumentStore
+        {
+            Urls = new[] { "http://127.0.0.1:8080" },
+            Database = "demo",
+            Conventions = 
+            {
+                FindCollectionName = type =>
+                {
+                    if (type == typeof(Employee))
+                        return "Workers";
+                    return DocumentConventions.DefaultGetCollectionName(type);
+                }
+            }
+        };
+
+        store.Initialize();
+
+        Employee john = new Employee();
+        john.FirstName = "John";
+        john.LastName = "Doe";
+
+        using var session = store.OpenSession();
+        session.Store(john);
+        session.SaveChanges();
+    }
+
+    public static void Individual()
+    {
+
+    }
+}
+
