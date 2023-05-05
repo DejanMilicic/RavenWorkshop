a5a25554d231b5bcab4bcf4350249872691b7c38
Dejan Milicic <dejan.milicic@gmail.com>
2023-05-05 10:01:25 +0200

    changing collection name - individual

--- Northwind/Northwind/Features/Customizations/CollectionName.cs
+++ Northwind/Northwind/Features/Customizations/CollectionName.cs
@@ -36,7 +36,18 @@ public static class CollectionName
 
     public static void Individual()
     {
+        IDocumentStore store = new DocumentStore { Urls = new[] { "http://127.0.0.1:8080" }, Database = "demo" }.Initialize();
 
+        Employee john = new Employee();
+        john.FirstName = "John";
+        john.LastName = "Doe";
+        
+        using var session = store.OpenSession();
+
+        var customCollectionName = "Workers";
+        session.Store(john);
+        session.Advanced.GetMetadataFor(john)["Raven-Entity-Name"] = customCollectionName;
+        session.SaveChanges();
     }
 }
 
