05708bc1fbac08ed44acfb4fb1850d72c2547897
Dejan Milicic <dejan.milicic@gmail.com>
2023-05-26 13:23:22 +0200

    todo

--- Northwind/Northwind/Features/Changes/Changes.cs
+++ Northwind/Northwind/Features/Changes/Changes.cs
@@ -1,19 +1,21 @@
 ﻿using System;
 
-namespace Northwind.Features.Changes
-{
-    public static class Changes
-    {
-        public static void Consume()
-        {
-            DocumentStoreHolder.Store.Changes()
-                .ForAllDocuments()
-                .Subscribe(change =>
-                {
-                    Console.WriteLine($"{change.Id} {change.Type}");
-                });
+namespace Northwind.Features.Changes;
 
-            Console.ReadLine();
-        }
+public static class Changes
+{
+    public static void Consume()
+    {
+        DocumentStoreHolder.Store.Changes()
+            .ForAllDocuments()
+            .Subscribe(change =>
+            {
+                Console.WriteLine($"{change.Id} {change.Type}");
+            });
+
+        Console.ReadLine();
     }
+
+    // TODO: examples for other cases (collection, index, ...)
+    // https://ravendb.net/docs/article-page/5.4/Csharp/client-api/changes/what-is-changes-api
 }
