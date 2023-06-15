b8b95a719705ce6eb40be1461d63f02eda57737d
Dejan Milicic <dejan.milicic@gmail.com>
2023-06-15 19:12:24 +0200

    All docs, statically typed

---
+++ Northwind/Northwind/Features/Indexes/AllDocuments_ByModifiedDate.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Raven.Client.Documents.Indexes;
+using System.Linq;
+
+namespace Northwind.Features.Indexes;
+
+public class AllDocuments_ByModifiedDate : AbstractIndexCreationTask<object, AllDocuments_ByModifiedDate.Entry>
+{
+    public class Entry
+    {
+        public DateTime ModifiedOn { get; set; }
+    }
+
+    public AllDocuments_ByModifiedDate()
+    {
+        Map = docs => 
+            from doc in docs 
+            select new Entry
+            {
+                ModifiedOn = MetadataFor(doc).Value<DateTime>("@last-modified")
+            };
+    }
+}
+
