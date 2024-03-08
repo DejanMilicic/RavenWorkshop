4a84bd104c600ab60d0e59ab295e3f34b9cfefe2
Dejan Milicic <dejan.milicic@gmail.com>
2024-03-08 09:39:29 +0100

    Product_Sales_ByDate polishing

--- Northwind/Northwind/Features/ArtificialDocuments/DailyProductSales/Product_Sales_ByDate.cs
+++ Northwind/Northwind/Features/ArtificialDocuments/DailyProductSales/Product_Sales_ByDate.cs
@@ -3,35 +3,34 @@ using System.Linq;
 using Northwind.Models.Entity;
 using Raven.Client.Documents.Indexes;
 
-namespace Northwind.Features.ArtificialDocuments.DailyProductSales
+namespace Northwind.Features.ArtificialDocuments.DailyProductSales;
+
+public class Product_Sales_ByDate : AbstractIndexCreationTask<Order, DailyProductSale>
 {
-    public class Product_Sales_ByDate : AbstractIndexCreationTask<Order, DailyProductSale>
+    public Product_Sales_ByDate()
     {
-        public Product_Sales_ByDate()
-        {
-            Map = orders => from order in orders
-                            from line in order.Lines
-                            select new
+        Map = orders => from order in orders
+                        from line in order.Lines
+                        select new DailyProductSale
+                        {
+                            Product = line.Product,
+                            Date = new DateTime(order.OrderedAt.Year, order.OrderedAt.Month, order.OrderedAt.Day),
+                            Count = 1,
+                            Total = line.Quantity * line.PricePerUnit * (1 - line.Discount)
+                        };
+
+        Reduce = results => from result in results
+                            group result by new { result.Product, result.Date } into g
+                            select new DailyProductSale
                             {
-                                line.Product,
-                                Date = new DateTime(order.OrderedAt.Year, order.OrderedAt.Month, order.OrderedAt.Day),
-                                Count = 1,
-                                Total = line.Quantity * line.PricePerUnit * (1 - line.Discount)
+                                Product = g.Key.Product,
+                                Date = g.Key.Date,
+                                Count = g.Sum(x => x.Count),
+                                Total = g.Sum(x => x.Total)
                             };
 
-            Reduce = results => from result in results
-                                group result by new { result.Product, result.Date } into g
-                                select new
-                                {
-                                    g.Key.Product,
-                                    g.Key.Date,
-                                    Count = g.Sum(x => x.Count),
-                                    Total = g.Sum(x => x.Total)
-                                };
-
-            OutputReduceToCollection = "DailyProductSales";
-            PatternReferencesCollectionName = "DailyProductSales/References";
-            PatternForOutputReduceToCollectionReferences = x => $"sales/daily/{x.Date:yyyy-MM-dd}";
-        }
+        OutputReduceToCollection = "DailyProductSales";
+        PatternReferencesCollectionName = "DailyProductSales/References";
+        PatternForOutputReduceToCollectionReferences = x => $"sales/daily/{x.Date:yyyy-MM-dd}";
     }
 }
