c46491174e46ed5a3b7617f950de56487819e2fe
Dejan Milicic <dejan.milicic@gmail.com>
2024-03-08 20:03:07 +0100

    reorganizing folders

--- Northwind/Northwind/Features/AdditionalSources/EmployeeUtil.cs
+++
@@ -1,7 +0,0 @@
-﻿public static class EmployeeUtil
-{
-    public static bool IsSpecial(string name)
-    {
-        return name == "Laura";
-    }
-}
---
+++ Northwind/Northwind/Features/AdditionalSources/SpecialEmployees/EmployeeUtil.cs
@@ -0,0 +1,7 @@
+﻿public static class EmployeeUtil
+{
+    public static bool IsSpecial(string name)
+    {
+        return name == "Laura";
+    }
+}
