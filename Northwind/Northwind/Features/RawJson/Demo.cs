738fdc7dafdee019941001f4178f3506b33ee282
Dejan Milicic <dejan.milicic@gmail.com>
2024-09-03 13:54:06 +0200

    refactored code to be more elegant

--- Northwind/Northwind/Features/RawJson/Demo.cs
+++ Northwind/Northwind/Features/RawJson/Demo.cs
@@ -1,66 +1,19 @@
 ﻿using Newtonsoft.Json.Linq;
-using Raven.Client.Documents;
-using Raven.Client.Documents.Commands.Batches;
 using Raven.Client.Documents.Session;
-using Sparrow.Json.Parsing;
-using System.Threading.Tasks;
-using Raven.Client.Documents.Commands;
 using Newtonsoft.Json;
 
 namespace Northwind.Features.RawJson;
 
-public class JsonHelper
-{
-    public static DynamicJsonValue ConvertJObjectToDynamicJsonValue(JObject jObject)
-    {
-        var djv = new DynamicJsonValue();
-
-        foreach (var property in jObject.Properties())
-        {
-            var value = property.Value;
-
-            if (value is JObject)
-                djv[property.Name] = ConvertJObjectToDynamicJsonValue((JObject)value);
-            else if (value is JArray)
-                djv[property.Name] = ConvertJArrayToDynamicJsonArray((JArray)value);
-            else
-                djv[property.Name] = value.ToObject<object>();
-        }
-
-        return djv;
-    }
-
-    private static DynamicJsonArray ConvertJArrayToDynamicJsonArray(JArray jArray)
-    {
-        DynamicJsonArray dja = new DynamicJsonArray();
-
-        foreach (var item in jArray)
-        {
-            if (item is JObject)
-                dja.Add(ConvertJObjectToDynamicJsonValue((JObject)item));
-            else if (item is JArray)
-                dja.Add(ConvertJArrayToDynamicJsonArray((JArray)item));
-            else
-                dja.Add(item.ToObject<object>());
-        }
-
-        return dja;
-    }
-}
-
 public static class RawJson
 {
-    public static void Demo2(IDocumentSession session, string id, string collection, string json)
+    public static void StoreRawJson(IDocumentSession session, string id, string collection, string json)
     {
         JObject parsedJson = JObject.Parse(json);
-
-        DynamicJsonValue djv = JsonHelper.ConvertJObjectToDynamicJsonValue(parsedJson);
-        djv["@metadata"] = new DynamicJsonValue { ["@collection"] = collection };
-        
-        session.Advanced.Defer(new PutCommandData(id, null, djv));
+        parsedJson["@metadata"] = new JObject { ["@collection"] = collection };
+        session.Store(parsedJson, id);
     }
 
-    public static string GetRawJsonDoc(IDocumentSession session, string id)
+    public static string GetRawJson(IDocumentSession session, string id)
     {
         return session.Load<JObject>(id).ToString(Formatting.None);
     }
