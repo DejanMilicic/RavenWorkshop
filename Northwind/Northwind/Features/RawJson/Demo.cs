4f2d49669ff8eb32f60d110c2ab9e5fa7a886198
Dejan Milicic <dejan.milicic@gmail.com>
2024-09-03 16:14:42 +0200

    System.Text examples

--- Northwind/Northwind/Features/RawJson/Demo.cs
+++ Northwind/Northwind/Features/RawJson/Demo.cs
@@ -1,4 +1,6 @@
 ﻿using Raven.Client.Documents.Session;
+using System.IO;
+using System.Collections.Generic;
 
 namespace Northwind.Features.RawJson;
 
@@ -18,8 +20,29 @@ public static class RawJson
         session.Store(parsedJson, id);
     }
 
-    public static string GetRawJson(IDocumentSession session, string id)
+    public static string GetRawJson1(IDocumentSession session, string id)
     {
         return session.Load<Newtonsoft.Json.Linq.JObject>(id).ToString(Newtonsoft.Json.Formatting.None);
+    }
+
+    public static string GetRawJson2(IDocumentSession session, string id)
+    {
+        using (var memoryStream = new MemoryStream())
+        {
+            session.Advanced.LoadIntoStream(new List<string>{ id }, memoryStream);
+            memoryStream.Position = 0; // Reset the stream position to the beginning
+            using (var reader = new StreamReader(memoryStream, System.Text.Encoding.UTF8))
+            {
+                string json = reader.ReadToEnd();
+                var jsonDocument = System.Text.Json.JsonDocument.Parse(json);
+                var result = jsonDocument.RootElement.GetProperty("Results")[0].ToString();
+
+                var resultObject = System.Text.Json.Nodes.JsonNode.Parse(result) as System.Text.Json.Nodes.JsonObject;
+
+                resultObject.Remove("@metadata");
+
+                return resultObject.ToJsonString(new System.Text.Json.JsonSerializerOptions { WriteIndented = true });
+            }
+        }
     }
 }
