63e22fd5851d0af0a7374098eeedd5ff9838d246
Dejan Milicic <dejan.milicic@gmail.com>
2024-09-03 15:49:14 +0200

    storing with both newtonsoft and system.text

--- Northwind/Northwind/Features/RawJson/Demo.cs
+++ Northwind/Northwind/Features/RawJson/Demo.cs
@@ -1,20 +1,25 @@
-﻿using Newtonsoft.Json.Linq;
-using Raven.Client.Documents.Session;
-using Newtonsoft.Json;
+﻿using Raven.Client.Documents.Session;
 
 namespace Northwind.Features.RawJson;
 
 public static class RawJson
 {
-    public static void StoreRawJson(IDocumentSession session, string id, string collection, string json)
+    public static void StoreRawJson1(IDocumentSession session, string id, string collection, string json)
     {
-        JObject parsedJson = JObject.Parse(json);
-        parsedJson["@metadata"] = new JObject { ["@collection"] = collection };
+        var parsedJson = Newtonsoft.Json.Linq.JObject.Parse(json);
+        parsedJson["@metadata"] = new Newtonsoft.Json.Linq.JObject { ["@collection"] = collection };
+        session.Store(parsedJson, id);
+    }
+
+    public static void StoreRawJson2(IDocumentSession session, string id, string collection, string json)
+    {
+        var parsedJson = System.Text.Json.Nodes.JsonNode.Parse(json);
+        parsedJson["@metadata"] = new System.Text.Json.Nodes.JsonObject { ["@collection"] = collection };
         session.Store(parsedJson, id);
     }
 
     public static string GetRawJson(IDocumentSession session, string id)
     {
-        return session.Load<JObject>(id).ToString(Formatting.None);
+        return session.Load<Newtonsoft.Json.Linq.JObject>(id).ToString(Newtonsoft.Json.Formatting.None);
     }
 }
