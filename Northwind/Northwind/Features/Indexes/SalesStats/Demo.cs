1fe9f9d0d830c6a27a125cadfb9e0ccefba1470e
Dejan Milicic <dejan.milicic@gmail.com>
2024-10-17 10:45:13 +0200

    Sales stats example finished

--- Northwind/Northwind/Features/Indexes/SalesStats/Demo.cs
+++ Northwind/Northwind/Features/Indexes/SalesStats/Demo.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using Raven.Client.Documents;
 using Raven.Client.Documents.Indexes;
@@ -51,6 +52,41 @@ public static class SalesStats
             LoadStatus = "In Progress"
         });
 
+        session.Store(new SalesRecord
+        {
+            User = "Martha",
+            Timestamp = new DateTime(2024, 10, 15, 5, 1, 1),
+            RecievedAmount = 5680,
+            PaidAmount = 0,
+            LoadStatus = "Done"
+        });
+
         session.SaveChanges();
+    }
+
+    private static void PrintResults(string heading, List<Sales_Aggregations.Entry> results)
+    {
+        Console.WriteLine("\n" + heading);
+
+        foreach (var result in results)
+        {
+            Console.WriteLine($"User: {result.User}, Timeframe: {result.Timeframe}, Timestamp: {result.Timestamp}, Receivables: {result.Receivables}, Payables: {result.Payables}, Balance: {result.Balance}");
+        }
+    }
+
+    public static void Query()
+    {
+        using var session = GetStore().OpenSession();
+
+        PrintResults("Sales aggregations for \"Georg\" in 4th Quarter of 2024",
+            session.Query<Sales_Aggregations.Entry, Sales_Aggregations>()
+            .Where(x => x.User == "Georg" && x.Timeframe == "Quarter" && x.Timestamp == "2024-4")
+            .ToList());
+
+        PrintResults("Sales aggregations for October of 2024, ordered by Receivables aggregation",
+            session.Query<Sales_Aggregations.Entry, Sales_Aggregations>()
+            .Where(x => x.Timeframe == "Month" && x.Timestamp == "2024-10")
+            .OrderByDescending(x => x.Receivables)
+            .ToList());
     }
 }
